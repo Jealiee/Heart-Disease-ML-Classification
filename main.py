5f9808ecb643f3e1d569e07231f0ddecacb165f9
tymcia <tymcia1710@gmail.com>
2026-04-15 17:13:21 +0200

    Add preprocessing and model functions to main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from data_exploration import explore_data
 from pathlib import Path
+from preprocessing import preprocessed_data
+from model import run_model
 
 #Put the csv file path here (if using different format change pd.read_csv to correct one)
 FILE_PATH = Path("heart_disease_uci.csv")
@@ -8,5 +10,7 @@ FILE_PATH = Path("heart_disease_uci.csv")
 df = pd.read_csv(FILE_PATH)
 
 if __name__ == "__main__":
+    preprocessed_data(df)
     explore_data(df, target ='num')
+    run_model(df)
 
