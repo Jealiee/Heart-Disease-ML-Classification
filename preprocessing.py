87c48b4dc62bf3f29e5ae5b70c9af8e4116cceab
tymcia <tymcia1710@gmail.com>
2026-04-16 15:12:55 +0200

    Refactor preprocessed_data function with filtering

--- preprocessing.py
+++ preprocessing.py
@@ -1,26 +1,32 @@
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+def preprocessed_data(df, only_cleveland=False):
 
-def preprocessed_data(df):
-    # Drop useless or high-missing columns
+    df = df.copy()
+
+    # Optional filtering
+    if only_cleveland:
+        df = df[df['dataset'] == 'Cleveland'].reset_index(drop=True)
+        # drop only id
+        df = df.drop(columns=['id'])
+
+        # encode sex
+        df['sex'] = df['sex'].map({'Male': 1, 'Female': 0})
+
+        return df
+
+    # Drop columns
     df = df.drop(columns=['id', 'ca', 'thal', 'slope'])
-    df = df.dropna(subset=['fbs', 'exang'])
 
-    # Fix impossible values
+    # Fix values
     df['trestbps'] = df['trestbps'].replace(0, np.nan)
     df['chol'] = df['chol'].replace(0, np.nan)
 
-    # Fill numerical columns with MEDIAN
+    # Fill numerical
     num_cols = ['age', 'trestbps', 'chol', 'thalch', 'oldpeak']
     for col in num_cols:
         df[col].fillna(df[col].median(), inplace=True)
 
-    # coding (features to 0/1)
-    df['sex'] = df['sex'].map({'M': 1, 'F': 0})
-
-    # 4. Fill categorical columns with MODE - that is a maybe can mess with the results and data
-    # cat_cols = ['sex', 'dataset', 'cp', 'fbs', 'restecg', 'exang', 'slope']
-    # for col in cat_cols:
-        # df[col].fillna(df[col].mode()[0], inplace=True)
+    # Encode
+    df['sex'] = df['sex'].map({'Male': 1, 'Female': 0})
 
     return df
