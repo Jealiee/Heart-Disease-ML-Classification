522e2854b2a2ec9d54106b88cd2592790b515e93
Jealie <adamus_julia@wp.pl>
2026-04-16 21:44:19 +0200

    Fixes, encoding and basic structure design

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from pathlib import Path
 from preprocessing import preprocessed_data
 from model import run_model
 
-#Put the csv file path here (if using different format change pd.read_csv to correct one)
+# Put the csv file path here (if using different format change pd.read_csv to correct one)
 FILE_PATH = Path("heart_disease_uci.csv")
 
 df = pd.read_csv(FILE_PATH)
+target = "num"
 
 if __name__ == "__main__":
-    df_full = preprocessed_data(df)
-    df_cleveland = preprocessed_data(df, only_cleveland=True)
+    # df_full = preprocessed_data(df)
+    # df_cleveland = preprocessed_data(df, only_cleveland=True)
     ## choose one
-    #explore_data(df_full, target='num')  # raw
-    explore_data(df_cleveland, target='num')  # filtered
+    explore_data(df, target)
+    # explore_data(df_full, target)  # raw
+    # explore_data(df_cleveland, target)  # filtered
     # run_model(df)
--- preprocessing.py
+++ preprocessing.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-# Setting only_cleaveland as true removes ~60% of the data but all features are preserved. 
+
+# Setting only_cleaveland as true removes ~60% of the data but all features are preserved.
 # Default value =false deletes 3 features with >30% missing data.
-def preprocessed_data(df, only_cleveland=False):
+def preprocessed_data(df, target, only_cleveland=False):
 
     df = df.copy()
 
     if only_cleveland:
         df = df[df["dataset"] == "Cleveland"].reset_index(drop=True)
 
-        #TODO: normalize columns ca, thal and slope
+        # TODO: normalize columns ca, thal and slope
 
     else:
         df = df.drop(columns=["ca", "thal", "slope"])
@@ -20,12 +21,32 @@ def preprocessed_data(df, only_cleveland=False):
     df["trestbps"] = df["trestbps"].replace(0, np.nan)
     df["chol"] = df["chol"].replace(0, np.nan)
 
+    # !!! MOVE TO SEP FUNCTION - DATA LEAKAGE RISK
     # Replace missing numerical values with median
-    num_cols = ["age", "trestbps", "chol", "thalch", "oldpeak"]
+    num_cols = df.select_dtypes(include=np.number).columns.tolist()
     for col in num_cols:
-        df[col].fillna(df[col].median(), inplace=True)
+        df[col] = df[col].fillna(df[col].median())
 
-    #TODO: Finish normalization
-    df["sex"] = df["sex"].map({"Male": 1, "Female": 0})
+    # Encoding boolean / binary values
+    df["sex"] = (
+        df["sex"].astype(str).str.strip().str.lower().map({"male": 1, "female": 0})
+    )
+    df["fbs"] = (
+        df["fbs"].astype(str).str.strip().str.lower().map({"true": 1, "false": 0})
+    )
+    df["exang"] = (
+        df["exang"].astype(str).str.strip().str.lower().map({"true": 1, "false": 0})
+    )
+
+    # !!! MOVE TO SEP FUNCTION - DATA LEAKAGE RISK
+    # Imputing missing boolean values with most frequent class
+    df["fbs"] = df["fbs"].fillna(df["fbs"].mode()[0])
+    df["exang"] = df["exang"].fillna(df["exang"].mode()[0])
+
+    # Depending on method may have to move
+    # TODO: Handle nominal categorical variables
+
+    # !!! MOVE TO SEP FUNCTION - DATA LEAKAGE RISK
+    # TODO: Scale numerical values
 
     return df
