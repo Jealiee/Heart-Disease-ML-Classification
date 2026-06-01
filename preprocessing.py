Fix data leak

preprocessing.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import RobustScaler
 
 # Setting only_cleaveland as true removes ~60% of the data but all features are preserved.
 # Default value =false deletes 3 features with >30% missing data.
-def clean_data(df,only_cleveland=False):
+def clean_data(df, only_cleveland=False):
 
     df = df.copy()
     df = df.drop_duplicates()
@@ -22,7 +22,7 @@ def clean_data(df,only_cleveland=False):
     df["trestbps"] = df["trestbps"].replace(0, np.nan)
     df["chol"] = df["chol"].replace(0, np.nan)
     df["oldpeak"] = df["oldpeak"].clip(lower=0)
-    
+
     # Encoding boolean / binary values
     df["sex"] = (
         df["sex"].astype(str).str.strip().str.lower().map({"male": 1, "female": 0})
@@ -33,7 +33,7 @@ def clean_data(df,only_cleveland=False):
     df["exang"] = (
         df["exang"].astype(str).str.strip().str.lower().map({"true": 1, "false": 0})
     )
-    cols_to_fix = ['trestbps', 'chol']
+    cols_to_fix = ["trestbps", "chol"]
 
     for col in cols_to_fix:
         q1 = df[col].quantile(0.25)
@@ -43,13 +43,11 @@ def clean_data(df,only_cleveland=False):
         lower_limit = q1 - 1.5 * iqr
 
         # Cap the values
-        df[col] = np.where(df[col] > upper_limit, upper_limit,
-                           np.where(df[col] < lower_limit, lower_limit, df[col]))
-
-    # Fill missing numerical values with the median
-    numerical_cols = ['trestbps', 'chol', 'thalch', 'oldpeak']
-    for col in numerical_cols:
-        df[col] = df[col].fillna(df[col].median())
+        df[col] = np.where(
+            df[col] > upper_limit,
+            upper_limit,
+            np.where(df[col] < lower_limit, lower_limit, df[col]),
+        )
 
     df["num"] = (df["num"] > 0).astype(int)
 